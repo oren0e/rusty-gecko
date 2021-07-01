d24e4a82babb1faa6f3c056b0772dd69bb7b6a96
oren0e <countx@gmail.com>
2021-07-01 13:01:51 +0300

    Add simple response error variants

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -38,16 +38,25 @@ pub struct SimplePriceResponse {
 // check for error - returns SimplePrices (with no errors)
 //
 
-#[derive(Error, Debug, Deserialize)]
+#[derive(Error, Debug)]
 pub enum SimpleResponseError {
     #[error("No data in input")]
-    EmptyInput,
+    EmptyInputError,
 
     #[error("No such coin found! (`{0}`)")]
     UnknownCoinError(String),
 
     #[error("No such currency found! (`{0}`)")]
     UnknownCurrencyError(String),
+
+    #[error("HTTP Error")]
+    HttpError(#[from] reqwest::Error),
+
+    #[error("IO Error")]
+    IOError(#[from] std::io::Error),
+
+    #[error("Deserialization Error")]
+    DeserializationError(#[from] serde_json::Error),
 }
 
 impl SimplePriceResponse {
